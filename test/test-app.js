b7f8d11944dd2a3749a6c9bca753d352f3490e8c
vdemedes <vdemedes@gmail.com>
2014-12-03 12:49:29 +0100

    working tests

--- test/test-app.js
+++ test/test-app.js
@@ -6,23 +6,23 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
 
-describe('ronin:app', function () {
+describe('ronin', function () {
   before(function (done) {
+    this.timeout(10000);
+    
     helpers.run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
-      .withPrompt({
-        someOption: true
-      })
+      .withArguments('hello-world')
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'bower.json',
       'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      'index.js',
+      'bin/hello-world',
+      'commands/hello.js'
     ]);
   });
 });
